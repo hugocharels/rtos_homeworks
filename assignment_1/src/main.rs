40135605cffe8b33f33fe675b5d09ab6b015aed4
Hugo <93338995+hugocharels@users.noreply.github.com>
2024-10-16 13:32:59 +0200

    Add files via upload

--- assignment_1/src/main.rs
+++ assignment_1/src/main.rs
@@ -1,17 +1,3 @@
-mod parser;
-mod taskset;
-mod task;
-mod job;
-
 fn main() {
-	let matches = parser::get_parser().get_matches();
-	let scheduling_algorithm = matches.get_one::<String>("scheduling algorithm").unwrap();
-	let verbose = matches.get_flag("verbose");
-	let taskset_file = matches.get_one::<String>("taskset file").unwrap();
-
-	println!("Algorithm: {}", scheduling_algorithm);
-	if verbose {
-		println!("Verbose mode is enabled");
-	}
-	println!("Task set file: {}", taskset_file);
+    println!("Hello, world!");
 }
